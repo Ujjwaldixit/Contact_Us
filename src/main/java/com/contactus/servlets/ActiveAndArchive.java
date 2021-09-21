78ae11c0be5a801fd5bb5082edb999970bc33ccf
Ujjwal Dixit <ujjwal.dixit.17.1@mountblue.tech>
2021-09-21 22:47:52 +0530

    feat(Active And Archieve): created ActiveAndArchive servlet to handle archive status

---
+++ src/main/java/com/contactus/servlets/ActiveAndArchive.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.contactus.servlets;
+
+import com.contactus.connection.ConnectionProvider;
+import com.contactus.dao.ContactsDao;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author ASUS
+ */
+public class ActiveAndArchive extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try ( PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet ActiveAndArchive</title>");
+            out.println("</head>");
+            out.println("<body>");
+            
+            ContactsDao contact = new ContactsDao(ConnectionProvider.getConnection());
+            String buttonClicked = request.getParameter("button");
+            String emails[] = request.getParameterValues("check");
+
+            boolean isUpdate = false;
+            for (String email : emails) {
+                if (buttonClicked.equals("active")) {
+                    isUpdate = contact.updateArchiveStatus(email, false);
+                } else if (buttonClicked.equals("archive")) {
+                    isUpdate = contact.updateArchiveStatus(email, true);
+                }
+            }
+
+            if (isUpdate) {
+                response.sendRedirect("requests");
+            } else {
+                response.sendRedirect("error.jsp");
+            }
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
